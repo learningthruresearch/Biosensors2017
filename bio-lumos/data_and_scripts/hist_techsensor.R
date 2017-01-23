15905036cee1c5f51e9092c18371db73eb98ff10
quendil1 <samuel.churlaud@cri-paris.org>
2017-01-23 21:00:44 +0100

    commit

--- bio-lumos/data_and_scripts/hist_techsensor.R
+++
@@ -1,3 +0,0 @@
-mean1 <- c(29.601970586104475, 31.978474823230744, 33.05471168094102, 34.27997403098735)
-green <- c(70, 80, 90, 100)
-barplot(mean1, width = 1, col= "orange",  main="Color detection by TSC3200", xlab =" 605nm             607nm             608nm            610nm", ylab ="% of difference compared to black screen", ylim = c(0, 100))
