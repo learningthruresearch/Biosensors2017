d9fecd171fd79608c414f228494e60620b30891e
faureleon <leon.faure@cri-paris.org>
2017-02-01 15:25:29 +0100

    finalVersion
    youpi

--- HERTZ/Codes/Biological/databio.py
+++ HERTZ/Codes/Biological/databio.py
@@ -62,3 +62,128 @@ print(counting(freq60Hz))
 print(counting(freq250Hz))
 print(counting(freq2000Hz))
 
+cle = ['0 Hz', '2 Hz', '4 Hz', '8 Hz', '20 Hz']
+
+fig = plt.figure(1, figsize=(9, 6))
+ax = fig.add_subplot(111)
+bp = ax.boxplot([freq60Hz['60Hz-0diff'], freq60Hz['60Hz-2diff'], freq60Hz['60Hz-4diff'], freq60Hz['60Hz-8diff'], freq60Hz['60Hz-20diff']], positions = range(5), patch_artist=True)
+#lp = ax.plot(range(55, 85), range(55, 85), label = 'x=y')
+ax.set_xticklabels(cle)
+ax.get_xaxis().tick_bottom()
+ax.get_yaxis().tick_left()
+ax.set_ylim([-0.5,1.5])
+ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
+ax.set_title('Results for 60 Hz')
+ax.set_xlabel('Modulation in Hertz')
+ax.set_ylabel('Detection coefficient in AU (between 0 and 3)')
+ax.legend()
+
+## change outline color, fill color and linewidth of the boxes
+for box in bp['boxes']:
+    # change outline color
+    box.set( color= 'red', linewidth=2)
+    # change fill color
+    box.set( facecolor = '#1b9e77' )
+
+## change color and linewidth of the whiskers
+for whisker in bp['whiskers']:
+    whisker.set(color='red', linewidth=2)
+
+## change color and linewidth of the caps
+for cap in bp['caps']:
+    cap.set(color='red', linewidth=2)
+
+## change color and linewidth of the medians
+for median in bp['medians']:
+    median.set(color='red', linewidth=2)
+
+## change the style of fliers and their fill
+for flier in bp['fliers']:
+    flier.set(marker='+', color='red', alpha=0.5)
+
+
+
+fig1 = plt.figure(2, figsize=(9, 6))
+ax1 = fig1.add_subplot(111)
+bp1 = ax1.boxplot([freq250Hz['250Hz-0diff'], freq250Hz['250Hz-2diff'], freq250Hz['250Hz-4diff'], freq250Hz['250Hz-8diff'],freq250Hz['250Hz-20diff']], positions = range(5), patch_artist=True)
+#lp1 = ax1.plot(range(245, 275), range(245, 275), label = 'x=y')
+ax1.set_xticklabels(cle)
+ax1.get_xaxis().tick_bottom()
+ax1.get_yaxis().tick_left()
+ax1.set_ylim([-0.5,1.5])
+ax1.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
+ax1.set_title('Results for 250 Hz')
+ax1.set_xlabel('Modulation in Hertz')
+ax1.set_ylabel('Detection coefficient in AU (between 0 and 3)')
+ax1.legend()
+
+## change outline color, fill color and linewidth of the boxes
+for box in bp1['boxes']:
+    # change outline color
+    box.set( color='r', linewidth=2)
+    # change fill color
+    box.set( facecolor = '#1b9e77' )
+
+## change color and linewidth of the whiskers
+for whisker in bp1['whiskers']:
+    whisker.set(color='r', linewidth=2)
+
+## change color and linewidth of the caps
+for cap in bp1['caps']:
+    cap.set(color='r', linewidth=2)
+
+## change color and linewidth of the medians
+for median in bp1['medians']:
+    median.set(color='r', linewidth=2)
+
+## change the style of fliers and their fill
+for flier in bp1['fliers']:
+    flier.set(marker='+', color='r', alpha=0.5)
+
+
+
+fig2 = plt.figure(3, figsize=(9, 6))
+ax2 = fig2.add_subplot(111)
+bp2 = ax2.boxplot([freq2000Hz['2000Hz-0diff'], freq2000Hz['2000Hz-2diff'], freq2000Hz['2000Hz-4diff'], freq2000Hz['2000Hz-8diff'], freq2000Hz['2000Hz-20diff']], positions = range(5), patch_artist=True)
+#lp2 = ax2.plot(range(1995, 2025), range(1995, 2025), label = 'x=y')
+ax2.set_xticklabels(cle)
+ax2.get_xaxis().tick_bottom()
+ax2.get_yaxis().tick_left()
+ax2.set_ylim([-0.5,1.5])
+ax2.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
+ax2.set_title('Results for 2000 Hz')
+ax2.set_xlabel('Modulation in Hertz')
+ax2.set_ylabel('Detection coefficient in AU (between 0 and 3)')
+ax2.legend()
+
+## change outline color, fill color and linewidth of the boxes
+for box in bp2['boxes']:
+    # change outline color
+    box.set( color='r', linewidth=2)
+    # change fill color
+    box.set( facecolor = '#1b9e77' )
+
+## change color and linewidth of the whiskers
+for whisker in bp2['whiskers']:
+    whisker.set(color='r', linewidth=2)
+
+## change color and linewidth of the caps
+for cap in bp2['caps']:
+    cap.set(color='r', linewidth=2)
+
+## change color and linewidth of the medians
+for median in bp2['medians']:
+    median.set(color='r', linewidth=2)
+
+## change the style of fliers and their fill
+for flier in bp2['fliers']:
+    flier.set(marker='+', color='r', alpha=0.5)
+
+
+fig.savefig('electronic_60Hz.png')
+fig1.savefig('electronic_250Hz.png')
+fig2.savefig('electronic_2000Hz.png')
+
+
+
+plt.show()
