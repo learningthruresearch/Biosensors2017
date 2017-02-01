06112cf75221479f9b18438369ca2799ef104030
lnarbona <lara.narbona@cri-paris.org>
2017-02-01 15:23:47 +0100

    Update databio.py

--- HERTZ/Codes/Biological/databio.py
+++ HERTZ/Codes/Biological/databio.py
@@ -1,8 +1,40 @@
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+#fctn that creates a nested dictionary where the keys are the n° of subject and the ear
+def addvalues(dictionary):
+    for subject in subjects_ears.keys():
+        ears = ['left', 'right']                    # creates a list to allow the iteration
+        for ear in ears:
+            subjects_ear_data = subjects_ears.get(subject).get(ear) # takes the values for each ear for each subject
+            for j in dictionary.keys():
+                n = list()                          # creates a new list
+                for i in subjects_ear_data:         # iterates on the data of each subject's ear
+                    if j in i:                      # is the key of the dictionary is in the ear data (if the freq listened to was the key)
+                        n.append(i[5])              # append the response to the list 'n'
+                dictionary[j].append(n)             # appends the list as a value for the dict keys
+    return dictionary
+
+#fctn that equiparates "t"=1 and "o"=0. It makes then a coefficient of response: sums the responses of a subject[ear] and divides the sum by the number of responses.
+def counting(dictionary):
+    dictionary = addvalues(dictionary)
+    detect = {"t":1,"o":0}                          # equiparates "t"=1 and "o"=0
+    for key1 in dictionary.keys():                  # iterates on dictionary keys
+        data = []
+        for values in dictionary.get(key1):         # gets the trios or duos of responses
+            if len(values) >= 1:                    # valid data ?
+                for i in range(len(values)):
+                    values[i] = detect[values[i]]   # changes t and o for 1 and 0
+                data.append(sum(values)/len(values))# it sums the new values and divides it sum by the number of responses. 
+        dictionary[key1] = data                     # adds data to the key
+    return dictionary
+
 
 fichier = open('output.csv', 'rb')                   # opens the file
-subjects = dict()                                    # creates a dictionary for subjects
 a = fichier.readlines()                              # reads everyline of the file
+
+subjects = dict()                                    # creates a dictionary for subjects
 for i in range(1,len(a)):                            # loop to fill the dictionary
     data = str(a[i]).replace("\\n'", '').replace('"','').split(',')  # deletes the '\\n' and makes a list out of the values separed by a ','
     if len(data) >= 9:                               # valid data?
@@ -26,19 +58,7 @@ freq60Hz = {'60Hz-0diff':[],'60Hz-2diff':[],'60Hz-4diff':[], '60Hz-8diff':[], '6
 freq250Hz = {'250Hz-0diff':[],'250Hz-2diff':[],'250Hz-4diff':[],'250Hz-8diff':[],'250Hz-20diff':[]}
 freq2000Hz = {'2000Hz-0diff':[],'2000Hz-2diff':[],'2000Hz-4diff':[],'2000Hz-8diff':[],'2000Hz-20diff':[]}
 
-def addvalues(dictionary):
-    for subject in subjects_ears.keys():
-        ears = ['left', 'right']                    # creates a list to allow the iteration
-        for ear in ears:
-            subjects_ear_data = subjects_ears.get(subject).get(ear) # takes the values for each ear for each subject
-            for j in dictionary.keys():
-                n = list()                          # creates a new list
-                for i in subjects_ear_data:         # iterates on the data of each subject's ear
-                    if j in i:                      # is the key of the dictionary is in the ear data (if the freq listened to was the key)
-                        n.append(i[5])              # append the response to the list 'n'
-                dictionary[j].append(n)             # appends the list as a value for the dict keys
-    return dictionary
+print(counting(freq60Hz))
+print(counting(freq250Hz))
+print(counting(freq2000Hz))
 
-addvalues(freq60Hz)
-addvalues(freq250Hz)
-addvalues(freq2000Hz)
