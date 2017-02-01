fce675836dfd7e3333622f2c48e1628f2f8a0c3c
lnarbona <lara.narbona@cri-paris.org>
2017-02-01 12:46:24 +0100

    Update databio.py

--- HERTZ/Codes/Biological/databio.py
+++ HERTZ/Codes/Biological/databio.py
@@ -4,51 +4,45 @@ import numpy as np
 import pylab
 import re
 
-fichier = open('subject-0.csv', 'r')             #opens the file
-subjects = dict()                                #creates a dictionary for subjects
-for i in fichier:                                #loop to fill the dictionary 
-    data = fichier.readline().strip().split(',') #reads every line, deletes the '\n' and makes a list out of the values separed by a ','
-    if len(data) >= 9:                           #valid data?
-        subject = data[8]                        #Takes the number of subject
-        if subject not in subjects:              #Verify if the number of subject is a key for the dictionary
-            subjects[subject] = list()           #Adds the key to the list
-        subjects[subject].append(data)           #Adds the data of subject X as a value for its key.
+fichier = open('subject_false.csv', 'rb')            # opens the file
+subjects = dict()                                    # creates a dictionary for subjects
+a = fichier.readlines()                              # reads everyline of the file
+for i in range(1,len(a)):                            # loop to fill the dictionary
+    data = str(a[i]).replace("\\n'", '').split(',')  # deletes the '\\n' and makes a list out of the values separed by a ','
+    if len(data) >= 9:                               # valid data?
+        subjectID = data[8]                          # takes the number of subject
+        if subjectID not in subjects:                # verify if the number of subject is a key for the dictionary
+            subjects[subjectID] = list()             # adds the key to the list
+        subjects[subjectID].append(data)             # adds the data of subject X as a value for its key.
 
 subjects_ears = dict()
-for key in subjects.keys():
-    subjects_ears[key] = dict()
-    subjects_ears[key]['right'] = list()         #Create a nested dictionar with the key 'left' and 'right'
-    subjects_ears[key]['left'] = list()
-    for line in subjects.get(key):
-        if line[3] == 'left':                    # Verify if the ear the subject was using was the left one
-            subjects_ears[key]['left'].append(line)  #Adds the data as a valueto the 'left' key
-        else:
-            subjects_ears[key]['right'].append(line)
-#print(subjects_ears)
+for subject in subjects.keys():
+    subjects_ears[subject] = dict()
+    subjects_ears[subject]['right'] = list()             # create a nested dictionar with the keys 'left' and 'right' for every subject
+    subjects_ears[subject]['left'] = list()
+    for line in subjects.get(subject):
+        if line[3] == 'left':                        # verify if the ear the subject was using was the left one
+            subjects_ears[subject]['left'].append(line)  # adds the data as a 'left' key value
+        else:                                        # if not left ear:
+            subjects_ears[subject]['right'].append(line) # adds the data as a 'left' key value
 
+freq60Hz = {'60Hz-0diff':[],'60Hz-2diff':[],'60Hz-4diff':[], '60Hz-8diff':[], '60Hz-20diff':[]}         # creates adictionary for each frequency
+freq250Hz = {'250Hz-0diff':[],'250Hz-2diff':[],'250Hz-4diff':[],'250Hz-8diff':[],'250Hz-20diff':[]}
+freq2000Hz = {'2000Hz-0diff':[],'2000Hz-2diff':[],'2000Hz-4diff':[],'2000Hz-8diff':[],'2000Hz-20diff':[]}
 
-freq60Hz = {'60Hz-0diff':{'left':[],'right':[]},'60Hz-2diff':{'left':[],'right':[]},'60Hz-4diff':{'left':[],'right':[]},'60Hz-20diff':{'left':[],'right':[]}}
-freq250Hz = {'250Hz-0diff':{'left':[],'right':[]},'250Hz-2diff':{'left':[],'right':[]},'250Hz-4diff':{'left':[],'right':[]},'250Hz-20diff':{'left':[],'right':[]}}
-freq2000Hz = {'2000Hz-0diff':{'left':[],'right':[]},'2000Hz-2diff':{'left':[],'right':[]},'2000Hz-4diff':{'left':[],'right':[]},'2000Hz-20diff':{'left':[],'right':[]}} 
+def addvalues(dictionary):
+    for subject in subjects_ears.keys():
+        ears = ['left', 'right']                    # creates a list to allow the iteration
+        for ear in ears:
+            subjects_ear_data = subjects_ears.get(subject).get(ear) # takes the values for each ear for each subject
+            for j in dictionary.keys():
+                n = list()                          # creates a new list
+                for i in subjects_ear_data:         # iterates on the data of each subject's ear
+                    if j in i:                      # is the key of the dictionary is in the ear data (if the freq listened to was the key)
+                        n.append(i[5])              # append the response to the list 'n'
+                dictionary[j].append(n)             # appends the list as a value for the dict keys
+    return dictionary
 
-for key in subjects_ears.keys():
-    ears = ['left', 'right']
-    print("for ear in", ears)
-    for ear in ears:
-        print(ear)
-        print("f = subjects_ears.get(key).get(ear)")
-        f = subjects_ears.get(key).get(ear)
-        print("for i in f")
-        n = list()
-        for i in f:
-            print(i)
-            print("for j in freq60Hz.keys()")
-            for j in freq60Hz.keys():
-                print("if", j, "in", i)
-                if j in i:
-                    print("n.append(",i[5],")")
-                    n.append(i[5])
-                    print(n)
-        freq60Hz[j][ear].append(n)
-print(freq60Hz)
-
+addvalues(freq60Hz)
+addvalues(freq250Hz)
+addvalues(freq2000Hz)
