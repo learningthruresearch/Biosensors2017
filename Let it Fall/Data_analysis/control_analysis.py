bc087c2334afa5f0578382fcbfc7886b1001ccb7
lnarbona <lara.narbona@cri-paris.org>
2017-02-09 11:39:41 +0100

    update_area_control

---
+++ Let it Fall/Data_analysis/control_analysis.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from math import *
+import csv
+
+# Execute this program with Python2
+
+Inten = [0, 0, 0, 100, 100, 100]
+allratio = []
+
+def donnee(file):
+
+	a = []
+	with open(file, 'rb') as csvfile: #Open the file
+		reader = csv.reader(csvfile)
+		reader.next() #switch the first line
+		for row in reader:
+			b=row[1]
+			a.append(float(b))
+        for i in range(0,6,2):
+            if i < 1:
+                p = 0
+            else:
+                p = a[i]/(a[i+1]+a[i])
+            allratio.append(p)
+
+donnee('Results.csv')
+donnee('Results1.csv')
+
+
+#fig = plt.figure()
+plt.plot(Inten, allratio, 'g+', mew=3, ms=10)
+plt.xlim(-2, 102)
+plt.ylim(0, 1.1)
+plt.title("Controls: Development towards light or gravity in function of the intensity of light", size=23)
+plt.xlabel("Intensity of LED (%)", size=23)
+plt.ylabel("ratio", size=23)
+plt.show()
+#fig.savefig('Test1.png')
