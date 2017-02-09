9dd34b7b04defd3dd8ec7866382ab27aba6052c7
Elena Calamand <elena.calamand@ri-paris.org>
2017-02-09 11:46:35 +0100

    version2

--- Let it Fall/Arduino/BlinkLEDROUGE.ino
+++ Let it Fall/Arduino/BlinkLEDROUGE.ino
@@ -1,18 +1,31 @@
-//Constantes
-const int ledPin1 = 3;
-const int ledPin2 = 5;
-const int ledPin3 = 9;
-int valPWM = 64; //64 = 25%, 127 = 50%, 191 = 75%, 255 = 100%
+//Constants, here each Pin which will be used
+const int ledPin1 = 5;
+const int ledPin2 = 10;
+const int ledPin3 = 3;
+const int ledPin4 = 6;
+const int ledPin5 = 9;
+
+//Give the intensities for each LED : 64 = 25%, 127 = 50%, 191 = 75%, 255 = 100%
+int val1 = 255; //100%
+int val2 = 51; //20%
+int val3 = 102; //40%
+int val4 = 153; //60%
+int val5 = 204; //80%
 
 void setup() {     
-  // Initialise le Pin comme une sortie | Initialize the digital pin as an output with pinMode()
+  // Initialise each Pin as an output with pinMode()
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin2, OUTPUT);
 }
 
-void loop() {
-  analogWrite(ledPin1, valPWM);
-  analogWrite(ledPin2, valPWM);
-  analogWrite(ledPin3, valPWM);
+void loop() { // Give to each Pin a value, here an intensity
+  analogWrite(ledPin1, val1);
+  analogWrite(ledPin2, val2);
+  analogWrite(ledPin3, val3);
+  analogWrite(ledPin4, val4);
+  analogWrite(ledPin5, val5);
 }
+
+
+
