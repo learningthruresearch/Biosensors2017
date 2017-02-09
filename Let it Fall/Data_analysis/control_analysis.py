3b032d116788e8eb8bfe24496e0aaa7de02a8a13
lnarbona <lara.narbona@cri-paris.org>
2017-02-09 12:04:15 +0100

    update

--- Let it Fall/Data_analysis/control_analysis.py
+++ Let it Fall/Data_analysis/control_analysis.py
@@ -12,17 +12,17 @@ allratio = []
 def donnee(file):
 
 	a = []
-	with open(file, 'rb') as csvfile: #Open the file
+	with open(file, 'rb') as csvfile: # Open the file
 		reader = csv.reader(csvfile)
-		reader.next() #switch the first line
+		reader.next()                 # Switch the first line
 		for row in reader:
-			b=row[1]
-			a.append(float(b))
-        for i in range(0,6,2):
-            if i < 1:
-                p = 0
+			b=row[1]                  # Take the second row
+			a.append(float(b))        # Converts itin a float
+        for i in range(0,6,2):        # Iterates on the pair numbers between 0 and 6: 0, 2, 4
+            if i < 1:                 # If there's no area above or below the line
+                p = 0                 # make the ratio equal to zero
             else:
-                p = a[i]/(a[i+1]+a[i])
+                p = a[i]/(a[i+1]+a[i])  # make a pourcentage of the total area that is above the line
             allratio.append(p)
 
 donnee('Results.csv')
@@ -30,7 +30,7 @@ donnee('Results1.csv')
 
 
 #fig = plt.figure()
-plt.plot(Inten, allratio, 'g+', mew=3, ms=10)
+plt.plot(Inten, allratio, 'g+', mew=3, ms=10)   # mew = marker edge width, ms = marker size
 plt.xlim(-2, 102)
 plt.ylim(0, 1.1)
 plt.title("Controls: Development towards light or gravity in function of the intensity of light", size=23)
